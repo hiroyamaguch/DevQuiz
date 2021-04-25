2e1090ff58dfd31b5e6ba0b2b006a45279d98b96
hiroyamaguch <hiroyuki_yamaguchi@hotmail.com>
2021-04-24 22:58:55 -0300

    git commit -m "first commit"

---
+++ lib/challenge/widget/quiz/quiz_widget.dart
@@ -0,0 +1,49 @@
+import 'package:devquiz/challenge/widget/answer/anser_widget.dart';
+import 'package:devquiz/core/app_text_styles.dart';
+import 'package:devquiz/shared/models/answer_model.dart';
+import 'package:devquiz/shared/models/question_model.dart';
+import 'package:flutter/material.dart';
+
+class QuizWidget extends StatefulWidget {
+  final QuestionModel question;
+  final VoidCallback onChange;
+
+  const QuizWidget({
+    Key? key,
+    required this.onChange,
+    required this.question,
+  }) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel answers(int index) => widget.question.answers[index];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          SizedBox(height: 64),
+          Text(widget.question.title, style: AppTextStyles.heading),
+          SizedBox(height: 24),
+          for (var i = 0; i < widget.question.answers.length; i++)
+            AnswerWidget(
+              answer: answers(i),
+              disabled: indexSelected != -1,
+              isSelected: indexSelected == i,
+              onTap: () {
+                indexSelected = i;
+                widget.onChange();
+                setState(() {});
+              },
+            ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/home/home_repository.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+
+import 'package:devquiz/shared/models/quiz_model.dart';
+import 'package:devquiz/shared/models/user_model.dart';
+import 'package:flutter/services.dart';
+
+class HomeRepository {
+  Future<UserModel> getUser() async {
+    final response = await rootBundle.loadString("assets/database/users.json");
+    return UserModel.fromJson(response);
+  }
+
+  Future<List<QuizModel>> getQuizzes() async {
+    final response =
+        await rootBundle.loadString("assets/database/quizzes.json");
+    final map = jsonDecode(response) as Map;
+    final list = map["quizzes"] as List;
+    final quizzes = list.map((e) => QuizModel.fromMap(e)).toList();
+    return quizzes;
+  }
+}
